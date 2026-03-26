6b88cee95f15f2e0ea592b94f0e9b56500291992
Marco <m.hebischoutlook.com>
2026-03-26 22:41:36 +0100

    creates new cards after submitting form

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ if (questionCard) {
 const newCardForm = document.querySelector('[data-js="new-card__form"]');
 
 if (newCardForm) {
+  let newCardIndex = 0;
+
   newCardForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -38,5 +40,86 @@ if (newCardForm) {
 
     e.target.reset();
     //e.target["first-name"].focus();
+
+    // create new element
+    newCardIndex++;
+
+    const newQuestionCard = document.createElement("section");
+    newQuestionCard.classList.add(
+      "question-card",
+      "question-card--answer-hidden",
+    );
+    newQuestionCard.tabIndex = 0;
+    newQuestionCard.ariaLabel = "Question";
+    newQuestionCard.dataset.js = "question-card";
+
+    const newQuestionCardQuestion = document.createElement("h2");
+    newQuestionCardQuestion.id = `q${newCardIndex}h2`;
+    newQuestionCardQuestion.classList.add("question-card__question");
+    const newQuestionCardQuestionPre = document.createElement("pre");
+    newQuestionCardQuestionPre.textContent = data.questionInput;
+
+    newQuestionCardQuestion.append(newQuestionCardQuestionPre);
+
+    const newQuestionCardAnswer = document.createElement("div");
+    newQuestionCardAnswer.classList.add("question-card__answer");
+    newQuestionCardAnswer.dataset.js = "question-card__answer";
+
+    const newQuestionCardAnswerParagraph = document.createElement("p");
+    const newQuestionCardAnswerPre = document.createElement("pre");
+    newQuestionCardAnswerPre.textContent = data.answerInput;
+
+    newQuestionCardAnswerParagraph.append(newQuestionCardAnswerPre);
+    newQuestionCardAnswer.append(newQuestionCardAnswerParagraph);
+
+    const newQuestionCardToggleAnswerButton = document.createElement("button");
+    newQuestionCardToggleAnswerButton.type = "button";
+    newQuestionCardToggleAnswerButton.classList.add(
+      "question-card__toggle-answer-button",
+    );
+    newQuestionCardToggleAnswerButton.js =
+      "question-card__toggle-answer-button";
+
+    const newQuestionCardAnswerShowText = document.createElement("span");
+    newQuestionCardAnswerShowText.classList.add(
+      "question-card__answer-show-text",
+    );
+    newQuestionCardAnswerShowText.textContent = "Show answer";
+
+    const newQuestionCardAnswerHideText = document.createElement("span");
+    newQuestionCardAnswerHideText.classList.add(
+      "question-card__answer-hide-text",
+    );
+    newQuestionCardAnswerHideText.textContent = "Hide answer";
+
+    const newQuestionCardBookmark = document.createElement("button");
+    newQuestionCardBookmark.type = "button";
+    newQuestionCardBookmark.classList.add("question-card__bookmark");
+    newQuestionCardBookmark.js = "question-card__bookmark";
+
+    const newQuestionCardBookmarkIcon = document.createElement("span");
+    newQuestionCardBookmarkIcon.classList.add("question-card__bookmark-icon");
+
+    const newQuestionCardCategories = document.createElement("ul");
+    newQuestionCardCategories.classList.add("question-card__categories");
+
+    const newQuestionCardCategory = document.createElement("li");
+    newQuestionCardCategory.classList.add("question-card__category");
+    newQuestionCardCategory.textContent = data.tagInput;
+
+    newQuestionCardToggleAnswerButton.append(
+      newQuestionCardAnswerShowText,
+      newQuestionCardAnswerHideText,
+    );
+    newQuestionCardBookmark.append(newQuestionCardBookmarkIcon);
+    newQuestionCardCategories.append(newQuestionCardCategory);
+    newQuestionCard.append(
+      newQuestionCardQuestion,
+      newQuestionCardAnswer,
+      newQuestionCardToggleAnswerButton,
+      newQuestionCardBookmark,
+      newQuestionCardCategories,
+    );
+    document.querySelector("main").append(newQuestionCard);
   });
 }
